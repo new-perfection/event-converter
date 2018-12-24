fb30bd3df13c34b41147e9ae1000f741dc5945a8
Mike <mike@newperfection.ca>
2018-12-24 10:21:43 -0500

    Added hello function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as express from 'express'
+import * as cors from 'cors'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// const allowedOrigins = [
+//   'https://event-converter.firebaseapp.com'
+// ]
+//
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }))
+
+app.use(cors())
+
+app.post('/', (req, res) => {
+  res.status(200).send({
+    data: 'hello'
+  });
+})
+
+export const v1 = functions.https.onRequest(app)
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+// });
--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import firebase from 'firebase/app'
+import 'firebase/functions'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    firebase.initializeApp({
+      projectId: 'event-converter'
+    })
+
+    this.state = {
+      result: 'loading'
+    }
+  }
+
+  componentDidMount() {
+    const helloWorld = firebase.functions().httpsCallable('v1/')
+    helloWorld().then(result => {
+      console.log(result.data)
+      this.setState({ result: result.data })
+    }).catch(function (error) {
+      // Getting the Error details.
+      console.log(error.code)
+      console.log(error.message)
+      console.log(error.details)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        
+        {this.state.result}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
